0423a9426462d9fe2f631c93a3989487e02e06ee
K-Kizuku <kotani.kizuku@gmail.com>
2024-02-18 15:56:07 +0900

    fix

--- src/components/page/Live/index.tsx
+++ src/components/page/Live/index.tsx
@@ -22,49 +22,58 @@ export const Live: FC = () => {
 				userSig,
 				role: TRTC.TYPE.ROLE_ANCHOR,
 			});
+			//   trtc.switchRole(TRTC.TYPE.ROLE_ANCHOR);
 			const view = "local-video";
 			await trtc.startLocalVideo({ view });
 			await trtc.startLocalAudio();
+			//   const d = await fetch(
+			//     "https://tencentcloud-api-server-5uovba7yfa-an.a.run.app/mixStreaming"
+			//   );
+			//   console.log(d);
 		};
 		d();
 	};
 
-	const handleClickAudience = async () => {
-		const userSig = await getUserSigniture(myId);
+	//   const handleClickAudience = async () => {
+	//     const userSig = await getUserSigniture(myId);
 
-		trtc.enterRoom({
-			strRoomId: "hoge",
-			userSig,
-			scene: TRTC.TYPE.SCENE_LIVE,
-			sdkAppId,
-			userId: myId,
+	//     trtc.enterRoom({
+	//       strRoomId: "hoge",
+	//       userSig,
+	//       scene: TRTC.TYPE.SCENE_LIVE,
+	//       sdkAppId,
+	//       userId: myId,
 
-			role: TRTC.TYPE.ROLE_AUDIENCE,
-		});
+	//       role: TRTC.TYPE.ROLE_ANCHOR,
+	//     });
 
-		trtc.on(TRTC.EVENT.REMOTE_VIDEO_AVAILABLE, ({ userId, streamType }) => {
-			const view = `${userId}_${streamType}`;
-			const div = document.createElement("div");
-			div.id = `${userId}_${streamType}`;
+	//     trtc.on(TRTC.EVENT.REMOTE_VIDEO_AVAILABLE, ({ userId, streamType }) => {
+	//       const view = `${userId}_${streamType}`;
+	//       const div = document.createElement("div");
+	//       div.id = `${userId}_${streamType}`;
 
-			div.innerHTML = "ビデオだよ";
+	//       div.innerHTML = "";
 
-			document.body.appendChild(div);
-			console.error(view);
+	//       document.body.appendChild(div);
+	//       document.styleSheets[0].insertRule(
+	//         `#${userId}_${streamType} { display: none; }`
+	//       );
+	//       console.error(view);
 
-			trtc.startRemoteVideo({
-				userId,
-				streamType,
-				view,
-			});
-		});
-	};
+	//       trtc.startRemoteVideo({
+	//         userId,
+	//         streamType,
+	//         view,
+	//       });
+	//     });
+	//   };
 	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
-	useEffect(() => {
-		handleClickAudience();
-	}, []);
+	//   useEffect(() => {
+	//     handleClickAudience();
+	//   }, []);
 	return (
 		<div>
+			<div id="local-video" style={{ display: "none" }} />
 			<div id="hoge" />
 			<div className="flex flex-row gap-4">
 				<Player src={`${import.meta.env.VITE_HLS_URL}`} />
@@ -111,9 +120,9 @@ const Player: FC<Props> = ({ src }) => {
 							ref={videoRef}
 							controls
 							style={{ width: "80vw", aspectRatio: "16/9" }}
-						>
-							<track kind="captions" />
-						</video>
+						/>
+						{/* <track kind="captions" /> */}
+						{/* </video> */}
 					</div>
 				) : (
 					<div>お使いのブラウザでは動画再生をサポートしていません。</div>
